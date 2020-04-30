9d3f01c0cd7c38e7c38af2ce7ed582a44b3cb4c9
chenjs9@lenovo.com_mac <chenjs9@lenovo.com>
2020-04-30 15:06:26 +0800

    解决报表数据显示不全、添加报表滚动条

--- src/main.js
+++ src/main.js
@@ -30,6 +30,7 @@ require('echarts/lib/chart/bar')
 // 引入提示框和title组件
 require('echarts/lib/component/tooltip')
 require('echarts/lib/component/title')
+require('echarts/lib/component/dataZoom')
 
 const has = Vue.directive('has', {
   inserted: function (el, binding) {
--- src/pages/graph/servloglist.vue
+++ src/pages/graph/servloglist.vue
@@ -59,31 +59,40 @@ export default {
                 Xdata.push(key)
                 Ydata.push(obj[key])
             }
+            console.log(Ydata)
+            console.log(Xdata)
             myChart.setOption({
               title: { 
                   text: 'NL领域统计',
                   left:'center'
               },
-              tooltip: {},
+              tooltip: {
+                trigger: 'axis',
+                axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                    type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                }
+              },
               xAxis: {
-                  data: Xdata
+                  data: Xdata,
+                  axisLabel:{
+                      rotate:40
+                  }
               },
               yAxis: {
                 minInterval : 1
               },
               grid:{
-                    x:'15%',    //左上角x轴距盒子边框的距离
-                    y:60,        //左上角Y轴距盒子边框的距离
-                    x2:'15%',    //右下角x轴距盒子边框的距离
-                    y2:30,     //右下角Y轴距盒子边框的距离
-                    borderWidth:1
-                },
+                 x:'5%', //左上角x轴距盒子边框的距离
+                 y:'10%', //左上角Y轴距盒子边框的距离
+                  x2:'5%',//右下角x轴距盒子边框的距离
+                 y2:'15%',//右下角Y轴距盒子边框的距离
+                 borderWidth:1
+              },
               series: [{
                   name: '数据条数',
                   type: 'bar',
                   data: Ydata,
                   color:"#409eff",
-                  barWidth : 60,
                   itemStyle: {
                     normal: {
                         label: {
@@ -96,7 +105,16 @@ export default {
                         }
                     }
                   }
-              }]
+              }],
+              dataZoom: [{
+                    id: 'dataZoomX',
+                    type: 'slider',
+                    show: true,
+                    start: 0,
+                    end: 80,
+                    handleSize: 8
+                  }]
+            
             })
         })
     }
