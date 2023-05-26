d324b5451831fb13b8cc78a00d7ca267ea2b630c
chenjs <chenjs9@lenovo.com>
2023-05-26 14:20:36 +0800

    修改登录页内容

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,60 +1,69 @@
 <template>
     <div class="login">
-        <div class="login_background">
-            <img src="../../static/images/vehi.svg" alt="">
+        <div class="head">
+            <div class="head_logo">
+                <img src="../../static/images/logo_img.png" alt="" class="logo_img">
+            </div>
+            <span class="logo_txt">联想车载语音管理系统</span>
         </div>
-            <div class="login-box">
-                    <div class="loginn" v-if="loginShow">
-                        <span class="form-title">联想车载语音管理系统</span>
-                        <el-form :label-position="'left'" :model="loginForm" :rules="loginRules" ref="loginForm" @submit.native.prevent>
-                            <el-form-item prop="username" :error="loginUserErr">
-                                <el-input type="text" v-model.trim="loginForm.username" placeholder="用户名" prefix-icon="el-icon-user" auto-complete="off" clearable></el-input>
-                            </el-form-item>
-                            <el-form-item prop="password" :error="loginPsdErr">
-                                <el-input type="password" v-model.trim="loginForm.password" placeholder="密码" prefix-icon="el-icon-lock" auto-complete="off" clearable></el-input>
-                            </el-form-item>
-                            <el-form-item prop="imgCode" class="imgcode-item" :error="loginImgCodeErr">
-                                    <el-input type="text" v-model.trim="loginForm.imgCode" placeholder="验证码" auto-complete="off" clearable>
-                                        <i slot="prefix" class="el-input__icon codeimg">
-                                          <img src="../../static/images/code.svg">
-                                        </i>
-                                    </el-input>
-                                    <img :src="limgCode" class="img" @click="getImgCode()">
-                                    <div class="tooltiptext" v-if="loginStatus == '1'">
-                                        登录超时
-                                    </div>
-                            </el-form-item>
-                            <el-form-item class="btn_item">
-                                <el-button type="primary" native-type="submit" @click="loginSubmit('loginForm')" :loading="loginLoading">登录</el-button>
-                                <span native-type="submit" @click="register()" class="register_btn">注册</span>
-                            </el-form-item>
-                        </el-form>
-                    </div>
-                    <div class="register" v-else key="register">
-                        <span class="form-title">联想车载语音管理系统</span>
-                        <el-form :label-position="'left'" :model="regForm" :rules="regRules" ref="regForm" @submit.native.prevent>
-                            <el-form-item prop="username" :error="regUserErr">
-                                <el-input style="position:fixed; bottom:-9999px"></el-input>
-                                <el-input type="text" v-model.trim="regForm.username" placeholder="请输入用户名" prefix-icon="el-icon-user" auto-complete="off" clearable></el-input>
-                            </el-form-item>
-                            <el-form-item prop="password">
-                                <el-input style="position:fixed; bottom:-9999px"></el-input>
-                                <el-input v-model.trim="regForm.password" placeholder="请输入密码" prefix-icon="el-icon-lock" auto-complete="off" clearable></el-input>
-                            </el-form-item>
-                            <el-form-item prop="regCode" class="imgcode-item" :error="regCodeErr">
-                                <el-input type="text" v-model.trim="regForm.regCode" placeholder="验证码" auto-complete="off" clearable>
+        <div class="login-box">
+            <div class="login_background">
+                <img src="../../static/images/vehi.svg" alt="">
+            </div>
+            
+            <div class="login-content">
+                <span class="form-title">联想车载语音管理系统</span>
+                <div class="loginn" v-if="loginShow">
+                    <el-form :label-position="'left'" :model="loginForm" :rules="loginRules" ref="loginForm" @submit.native.prevent>
+                        <el-form-item prop="username" :error="loginUserErr">
+                            <el-input type="text" v-model.trim="loginForm.username" placeholder="用户名" prefix-icon="el-icon-user" auto-complete="off" clearable></el-input>
+                        </el-form-item>
+                        <el-form-item prop="password" :error="loginPsdErr">
+                            <el-input type="password" v-model.trim="loginForm.password" placeholder="密码" prefix-icon="el-icon-lock" auto-complete="off" clearable></el-input>
+                        </el-form-item>
+                        <el-form-item prop="imgCode" class="imgcode-item" :error="loginImgCodeErr">
+                                <el-input type="text" v-model.trim="loginForm.imgCode" placeholder="验证码" auto-complete="off" clearable>
                                     <i slot="prefix" class="el-input__icon codeimg">
                                         <img src="../../static/images/code.svg">
                                     </i>
                                 </el-input>
                                 <img :src="limgCode" class="img" @click="getImgCode()">
-                            </el-form-item>
-                            <el-form-item class="btn_item">
-                                <el-button type="primary" native-type="submit" @click="regSubmit('regForm')" :loading="regLoading">注册</el-button>
-                                <span native-type="submit" @click="register()" class="register_btn">登录</span>
-                            </el-form-item>
-                        </el-form>
-                    </div>
+                                <div class="tooltiptext" v-if="loginStatus == '1'">
+                                    登录超时
+                                </div>
+                        </el-form-item>
+                        <el-form-item class="btn_item lr-btn">
+                            <el-button type="primary" native-type="submit" @click="loginSubmit('loginForm')" :loading="loginLoading">登录</el-button>
+                            <span native-type="submit" @click="register()" class="register_btn">注册</span>
+                        </el-form-item>
+                    </el-form>
+                </div>
+                <div class="register" v-else key="register">
+                    <el-form :label-position="'left'" :model="regForm" :rules="regRules" ref="regForm" @submit.native.prevent>
+                        <el-form-item prop="username" :error="regUserErr">
+                            <el-input style="position:fixed; bottom:-9999px"></el-input>
+                            <el-input type="text" v-model.trim="regForm.username" placeholder="请输入用户名" prefix-icon="el-icon-user" auto-complete="off" clearable></el-input>
+                        </el-form-item>
+                        <el-form-item prop="password">
+                            <el-input style="position:fixed; bottom:-9999px"></el-input>
+                            <el-input v-model.trim="regForm.password" placeholder="请输入密码" prefix-icon="el-icon-lock" auto-complete="off" clearable></el-input>
+                        </el-form-item>
+                        <el-form-item prop="regCode" class="imgcode-item" :error="regCodeErr">
+                            <el-input type="text" v-model.trim="regForm.regCode" placeholder="验证码" auto-complete="off" clearable>
+                                <i slot="prefix" class="el-input__icon codeimg">
+                                    <img src="../../static/images/code.svg">
+                                </i>
+                            </el-input>
+                            <img :src="limgCode" class="img" @click="getImgCode()">
+                        </el-form-item>
+                        <el-form-item class="btn_item lr-btn">
+                            <el-button type="primary" native-type="submit" @click="regSubmit('regForm')" :loading="regLoading">注册</el-button>
+                            <span native-type="submit" @click="register()" class="register_btn">登录</span>
+                        </el-form-item>
+                    </el-form>
+                </div>
+            </div>
+                
         </div>
         
     </div>
@@ -261,48 +270,66 @@ export default {
 
 <style scoped>
 .login{
-    background-color: #3CCDFF;
-    background-image: linear-gradient(225deg,#3CCDFF 0%,#939BFF 50%,#65A8F0 100%);
-    background-image: -webkit-linear-gradient(225deg,#3CCDFF 0%,#939BFF 50%,#65A8F0 100%);
-    background-image: -moz-linear-gradient(225deg,#3CCDFF 0%,#939BFF 50%,#65A8F0 100%);
-    background-image: -o-linear-gradient(225deg,#3CCDFF 0%,#939BFF 50%,#65A8F0 100%);
+    background-color: #3ccdff;
+    background-image: linear-gradient(225deg,#3ccdff 0%,#93c4ff 50%,rgb(101, 189, 240) 100%);
     width: 100%;
     height: 100%;
     min-width: 1200px;
     display: flex;
     align-items: center;
     justify-content: space-around;
+    
 }
 .login_background{
-    width: 50%;
-    height: 99%;
+    width: 55%;
+    background-image: linear-gradient(225deg,#3CCDFF 0%,rgb(101, 189, 240) 50%,#93c4ff 100%);
+    text-align: center;
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+    box-shadow: rgba(60, 205, 255, 0.4) 4px 0px, rgba(60, 205, 255, 0.1) 8px 0px, rgba(60, 205, 255, 0.05) 12px 0px;
+    z-index: 99;
 }
 .login_background img{
-    width: 100%;
+    width: 80%;
     height: 100%;
 }
 .login-box {
     background: #fff;
-    padding: 20px 30px 5px;
-    border-radius: 10px;
-    width: 20%;
-    min-width: 270px;
-    max-width: 300px;
+    padding: 0;
+    border-radius: 22px;
+    width: 50%;
+    min-width: 650px;
+    max-width: 700px;
+    display: flex;
+    box-shadow: rgba(101, 189, 240, 1) 0px 0px 50px 10px, #93c4ff 0px 0px 50px 10px;
+}
+.login-content{
+    width: 45%;
+    padding: 20px 0 0;
 }
 
 .login-box .form-title{
     display: block;
     text-align: center;
-    padding-bottom: 30px;
-    font-size: 20px;
+    font-size: 18px;
+    background-image: linear-gradient(225deg,#65a7f0 0%,#93c4ff 50%,#3CCDFF 100%);
+    border-radius: 50px;
+    height: 50px;
+    line-height: 50px;
+    color: #fff;
+    width: 80%;
+    margin: 0 auto 30px;
+}
+.loginn, .register{
+    padding: 0 30px;
 }
 .login-box .btn_item .el-form-item__content button{
-    width: 100%;
+    width: 60%;
     border-radius: 20px;
 }
 .register_btn{
     display: block;
-    width: 100%;
+    width: 60%;
     background: #fff;
     border: 1px solid #dcdfe6;
     border-radius: 20px;
@@ -311,11 +338,33 @@ export default {
     height: 38px;
     line-height: 38px;
     cursor: pointer;
+    margin: 10px auto 0;
 }
+
 .fadeOutLeft-enter-active, .fadeOutLeft-leave-active {
   transition: opacity .1s;
 }
 .fadeOutLeft-enter, .fadeOutLeft-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
+}
+.login .head{
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    color: #fff;
+}
+.login .head_logo {
+    width: 40px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+}
+.login .head_logo .logo_img{
+    width: 100%;
+}
+.login .head .logo_txt{
+    padding-left: 10px;
 }
 </style>
